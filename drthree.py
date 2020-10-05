26a66c954fc44936623aa032bc72eac05f4c2bfa
Gerardo Méndez Alvarez <36525807+gera1013@users.noreply.github.com>
2020-10-05 13:36:13 -0600

    Update drthree.py

--- drthree.py
+++ drthree.py
@@ -5,14 +5,12 @@ import random
 
 brick = Material(diffuse = color(0.8, 0.25, 0.25), spec = 16)
 stone = Material(diffuse = color(0.4, 0.4, 0.4), spec = 4)
-mirror = Material(spec = 64, t = REFLECTIVE)
-glass = Material(spec = 64, ior = 1.5, t= TRANSPARENT) 
 
 width = 512
 height = 512
 r = RayTracer(width, height)
 
-r.point_light = PointLight(position = [0, 0, -7.9], intensity = 2.5)
+r.point_light = PointLight(position = [0, 0, -8], intensity = 2)
 r.ambient_light = AmbientLight(strength = 1)
 
 r.scene.append(Room([0, 0, -4.8], 5, stone))
